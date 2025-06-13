87bd9c971102a50de897cd7db8fbaa054b0cb3d6
dharun7405 <dharunsudha74@gmail.com>
2025-06-13 15:11:27 +0530

    Extracted reply from response

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -1,5 +1,7 @@
 package com.email.writer.app;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,9 +16,9 @@ public class EmailGeneratorService {
         this.webClient = webClient;
     }
 
-    @Value("${gemini.url}")
+    @Value("${gemini.api.url}")
     private String geminiApiUrl;
-    @Value("${gemini.api}")
+    @Value("${gemini.api.key}")
     private String geminiApiKey;
 
 
@@ -37,15 +39,28 @@ public class EmailGeneratorService {
         String response = webClient.post()
                 .uri(geminiApiUrl+geminiApiKey)
                 .header("Content-Type","application/json")
+                .bodyValue(requestBody)
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
-        
-        //RETURN RESPONSE
+
+        //RETURN EXTRACTED RESPONSE
         return extractResponseContent(response);
     }
 
     private String extractResponseContent(String response) {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response);
+            return rootNode
+                    .path("candidates").get(0)
+                    .path("content")
+                    .path("parts").get(0)
+                    .path("text")
+                    .asText();
+        }catch (Exception e){
+            return "Error processing request "+e.getMessage();
+        }
     }
 
     private String buildPrompt(EmailRequest emailRequest) {
