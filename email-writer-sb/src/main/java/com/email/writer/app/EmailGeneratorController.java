0f03f00bc285ba4be15f9e72f3718a15d894d9c4
dharun7405 <dharunsudha74@gmail.com>
2025-06-13 15:27:59 +0530

    Crossorigin added

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -2,13 +2,11 @@ package com.email.writer.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin
 public class EmailGeneratorController {
 
     @Autowired
