6816427c4c6443e744686b65bdf1391c4646589a
dharun7405 <dharunsudha74@gmail.com>
2025-06-13 14:09:34 +0530

    Prompt built

---
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -0,0 +1,25 @@
+package com.email.writer.app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailGeneratorService {
+
+    public String generateEmailReply(EmailRequest emailRequest){
+        //BUILD THE PROMPT
+        String prompt = buildPrompt(emailRequest);
+        //CRAFT A REQUEST
+        //DO REQUEST AND GET RESPONSE
+        //RETURN RESPONSE
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line");
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone");
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
+}
